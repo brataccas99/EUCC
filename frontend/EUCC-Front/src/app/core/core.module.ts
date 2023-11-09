1f814762ef0022e5dc28373ef8bd122dbfec4b0d
gabriele <gabriele.lodato99@gmail.com>
2023-11-09 23:59:56 +0100

    added navbar disappearing while scrolling

--- frontend/EUCC-Front/src/app/core/core.module.ts
+++ frontend/EUCC-Front/src/app/core/core.module.ts
@@ -6,10 +6,16 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FeaturesComponent } from './features/features.component';
 import { IconsModule } from '../shared/icons/icons.module';
 import { CarouselComponent } from '../shared/carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent, FeaturesComponent, CarouselComponent],
+  declarations: [
+    NavbarComponent,
+    FeaturesComponent,
+    CarouselComponent,
+    FooterComponent,
+  ],
   imports: [CommonModule, CoreRoutingModule, IconsModule],
-  exports: [NavbarComponent, FeaturesComponent],
+  exports: [NavbarComponent, FeaturesComponent, FooterComponent],
 })
 export class CoreModule {}
--- frontend/EUCC-Front/src/app/core/navbar/navbar.component.ts
+++ frontend/EUCC-Front/src/app/core/navbar/navbar.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  private lastScrollTop = 0;
 
+  constructor() {
+    window.addEventListener('scroll', () => {
+      this.detectScrollDirection();
+    });
+  }
+
+  private detectScrollDirection() {
+    const st = window.pageYOffset;
+
+    if (st > this.lastScrollTop) {
+      // Scrolling down
+      this.addScrollingClass();
+    } else {
+      // Scrolling up
+      this.addScrollingUpClass();
+    }
+
+    this.lastScrollTop = st;
+  }
+
+  private addScrollingClass() {
+    const element = document.querySelector('.smart-scroll');
+    element!.classList.remove('scrolling-up');
+    element!.classList.add('scrolling');
+  }
+
+  private addScrollingUpClass() {
+    const element = document.querySelector('.smart-scroll');
+    element!.classList.remove('scrolling');
+    element!.classList.add('scrolling-up');
+  }
 }
